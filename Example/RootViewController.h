d6b7a730ce418179e3c0d2cf099d4d43df80ca42
Armin <armin@jtribe.com.au>
2013-07-09 14:34:47 +1000

    git ignores now cocoapods

---
+++ Example/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Cards
+//
+//  Created by Armin Kroll on 9/07/13.
+//  Copyright (c) 2013 jTribe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ Example/RootViewController.m
@@ -0,0 +1,38 @@
+//
+//  RootViewController.m
+//  Cards
+//
+//  Created by Armin Kroll on 9/07/13.
+//  Copyright (c) 2013 jTribe. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
