91f32ec169646528704081f94772812955649863
Armin <armin@jtribe.com.au>
2013-07-19 15:12:17 +1000

    added window to app delegate

--- Example/AppDelegate.m
+++ Example/AppDelegate.m
@@ -13,6 +13,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  
   // Override point for customization after application launch.
   
   UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:[[RootViewController alloc] init]];
